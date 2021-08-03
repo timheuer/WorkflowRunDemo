9aed7458170bdaa3cad323bf757052c92f9b3fec
Tim Heuer <tim@timheuer.com>
2021-08-03 13:51:39 -0700

    Test

--- WorkflowRunDemo/Program.cs
+++ WorkflowRunDemo/Program.cs
@@ -6,7 +6,7 @@ namespace WorkflowRunDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from .NET");
         }
     }
 }
